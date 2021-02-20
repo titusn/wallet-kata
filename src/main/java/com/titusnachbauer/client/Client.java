0ee65ca1b9ffb2747c6a4a5140c3ac9981ae3ab7
titusn <mail@titusnachbauer.com>
2021-02-20 16:38:27 +0100

    Clean up

--- src/main/java/com/titusnachbauer/client/Client.java
+++ src/main/java/com/titusnachbauer/client/Client.java
@@ -9,12 +9,11 @@ public class Client {
     public static final int HTTP_STATUS_OK = 200;
     private OkHttpClient httpClient = new OkHttpClient();
 
-    public int getRequest(URL url) throws IOException {
+    public Response getRequest(URL url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
         Call call = httpClient.newCall(request);
-        Response response = call.execute();
-        return response.code();
+        return call.execute();
     }
 }
--- src/test/java/APIClientTest.java
+++ src/test/java/APIClientTest.java
@@ -2,13 +2,25 @@ import com.titusnachbauer.client.Client;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 class APIClientTest {
+    private final String BASE_URL = "https://cloud.iexapis.com/v1";
+    Client client = new Client();
+
     @Test
-    void givenStatusURLThenResponseShouldBeOK() throws IOException {
-        Client client = new Client();
-        Assertions.assertEquals(Client.HTTP_STATUS_OK, client.getRequest(new URL("https://cloud.iexapis.com/v1/status")));
+    void givenStatusURLThenResponseShouldBeOK() throws Exception {
+        Assertions.assertEquals(Client.HTTP_STATUS_OK, client.getRequest(new URL(BASE_URL +"/status")).code());
+    }
+
+    @Test
+    void givenStatusURLThenResponseShoulBeJSON() throws Exception {
+        String contentType = Objects.requireNonNull(
+                Objects.requireNonNull(
+                client.getRequest(new URL(BASE_URL + "/status")).body())
+                .contentType())
+                .toString();
+        Assertions.assertEquals("application/json; charset=utf-8", contentType);
     }
 }
