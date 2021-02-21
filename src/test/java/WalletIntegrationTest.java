79798099669628c0c4439460a42cd0d1aa48710c
titusn <mail@titusnachbauer.com>
2021-02-21 16:35:30 +0100

    Add test for exception

--- src/test/java/WalletIntegrationTest.java
+++ src/test/java/WalletIntegrationTest.java
@@ -1,9 +1,7 @@
-import com.titusnachbauer.wallet.IEXRateProvider;
-import com.titusnachbauer.wallet.RateProvider;
-import com.titusnachbauer.wallet.Stock;
-import com.titusnachbauer.wallet.Wallet;
+import com.titusnachbauer.wallet.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WalletIntegrationTest {
@@ -14,6 +12,12 @@ class WalletIntegrationTest {
     void givenWalletWithOneStockWhenCalledShouldReturnCurrentValue() {
         Wallet wallet = new Wallet(new Stock(2, "AAPL"), iexRateProvider);
         assertTrue(wallet.computeValue() > 0.0);
+    }
+
+    @Test
+    void givenWalletWithUnknownStockTypeWhenCalledShouldThrowTickerSymbolUnknown() {
+        wallet.add(new Stock(1, "THISISNOTATICKERSYMBOL"));
+        assertThrows(TickerSymbolNotFound.class, wallet::computeValue);
     }
 
 }
