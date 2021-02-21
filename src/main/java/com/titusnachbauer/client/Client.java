84074b353e0efd2fa3a43a841677acd5763ff2ab
titusn <mail@titusnachbauer.com>
2021-02-21 14:50:27 +0100

    Remove unused method and field

--- src/main/java/com/titusnachbauer/client/Client.java
+++ src/main/java/com/titusnachbauer/client/Client.java
@@ -1,32 +1,18 @@
 package com.titusnachbauer.client;
 
 import com.titusnachbauer.service.QuoteDto;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class Client {
     public static final int HTTP_STATUS_OK = 200;
     public static final String BASE_URL = "https://cloud.iexapis.com/v1/";
     private final String publishToken;
-    private final OkHttpClient httpClient = new OkHttpClient();
 
     public Client(String iexPublishToken) {
         publishToken = iexPublishToken;
-    }
-
-    public Response getRequest(URL url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        Call call = httpClient.newCall(request);
-        return call.execute();
     }
 
     public QuoteDto getQuote(String symbol) throws IOException {
