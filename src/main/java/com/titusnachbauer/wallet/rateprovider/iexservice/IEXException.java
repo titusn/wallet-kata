9ebb20f1d0cb5849d41f5c33ffc13a17ac95be45
titusn <mail@titusnachbauer.com>
2021-03-04 20:19:37 +0100

    Add IEXException

---
+++ src/main/java/com/titusnachbauer/wallet/rateprovider/iexservice/IEXException.java
@@ -0,0 +1,7 @@
+package com.titusnachbauer.wallet.rateprovider.iexservice;
+
+public class IEXException extends RuntimeException {
+    public IEXException(String message) {
+        super(message);
+    }
+}
